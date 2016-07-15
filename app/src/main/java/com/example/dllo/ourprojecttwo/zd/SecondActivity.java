16d7d20272cafd307615181ac0e7c1299098b8c0
dllo <dllo@wangjiandeMac-mini-4.local>
2016-07-15 11:53:34 +0800

    已经改动了

---
+++ app/src/main/java/com/example/dllo/ourprojecttwo/zd/SecondActivity.java
@@ -0,0 +1,9 @@
+package com.example.dllo.ourprojecttwo.zd;
+
+import android.app.Activity;
+
+/**
+ * Created by dllo on 16/7/15.
+ */
+public class SecondActivity extends Activity {
+}
--- app/src/main/java/com/example/dllo/ourprojecttwo/zgh/MainActivity.java
+++ app/src/main/java/com/example/dllo/ourprojecttwo/zgh/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.dllo.ourprojecttwo;
+package com.example.dllo.ourprojecttwo.zgh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.dllo.ourprojecttwo.R;
 
 public class MainActivity extends AppCompatActivity {
 
